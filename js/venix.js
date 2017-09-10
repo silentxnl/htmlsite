09b138e96ef5950f2524d63a4434b8f92f6e8878
SilentX <silentx@gmail.com>
2017-09-10 16:00:20 +0200

    update

--- js/venix.js
+++ js/venix.js
@@ -6,3 +6,37 @@ var SiteName = "Venix Logix B.V.";
 
 var email = "test";
 var value = "test";
+
+
+
+angular.module('venix', [])
+
+.controller('VenixController', function($scope) {
+
+    $scope.sitenameVNX = 'Venix Logix B.V. - Freight Forwarders';
+    $scope.sitename2 = 'nerd';
+
+    $scope.CompanyName = 'Venix Logix B.V.';
+    $scope.address1 = 'Frobenstraat 19C';
+    $scope.address2 = '3045RD Rotterdam';
+    $scope.country = 'the Netherlands;'
+    $scope.MailAddress = 'info@venixlogix.eu';
+    $scope.PhoneNr = '+31.10.480.4292';
+
+    $scope.copy1 = 'VENIX LOGIX BV 2017. All RIGHTS RESERVED.';
+    $scope.copy2 = 'DESIGNED AND DEVELOPED BY VINCENT VAN BERKEL';
+
+    $scope.copyright = 'Copyright (c) 2017 Vincent van Berkel';
+
+    $scope.beer = '1';
+    var sitename3;
+    sitename3 = 'Venix';
+    sitename= 'aap';
+ 
+
+    
+  })
+
+//var myFirstModule = angular.module("myFirstModule ", []);
+
+//myFirstModule.controller("MyController", function);	
